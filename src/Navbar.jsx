a6a205804e931f5f3b50c6879ee37310e826d237
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-06-01 13:06:17 +0000

    addingnavbarhtml

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export const Navbar = () => {
+
+    return (
+        <>
+          <div>
+            <div>
+                <h1>Title</h1> 
+            </div>
+            <div>
+                <img src="https://img.icons8.com/?size=512&id=9720&format=png" alt="cart" />
+                <span>0</span>
+            </div>
+            </div>
+        </>
+
+    )
+
+
+}
