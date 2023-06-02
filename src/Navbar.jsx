92500ded5444a8390e8d1f1b55bb84f4717c0eea
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-06-02 11:47:08 +0000

    12

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
                 <h1>Title</h1> 
             </div>
             <div>
-                <img src="https://img.icons8.com/?size=512&id=9720&format=png" alt="cart" />
+                <img src="" alt="cart" />
                 <span>0</span>
             </div>
             </div>
